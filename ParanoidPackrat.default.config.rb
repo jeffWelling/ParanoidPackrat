b5956ea11fd13703b80e465500f715f96103781b
Jeff Welling <Jeff.Welling@Gmail.com>
2009-09-19 16:24:52 -0700

    corrected config examples, added another example
    Corrected the examples to use PPackratConfig instead of config
    which was deprecated when we turned PPackratConfig into a Module.
    Also, added an example for a single file backup.

--- ParanoidPackrat.default.config.rb
+++ ParanoidPackrat.default.config.rb
@@ -16,16 +16,19 @@
     along with ParanoidPackrat.  If not, see <http://www.gnu.org/licenses/>.
 =end
 require 'classes/PPackratConfig.rb'
-config=PPackratConfig.new
+
+#This line sets a Global backup destination, which will be defaulted to in the event
+#of no backup destination being specified on the per backup name basis.
+#PPackratConfig.setBackupDestination "/mnt/sdg"
 
 #Backup your home directory to /mnt/sdi/
 #
 #This will create a directory called 'backup' in /mnt/sdi if it does
 #not yet exist, it will then create a directory named /mnt/sdi/backup/jeff's\ home/,
 #so as to have backups accessibly by name, and will put your backups in there sorted by date.
-#config.addName "jeff's home"
-#config.setBackupTarget "jeff's home", "/home/jeff"
-#config.setBackupDestinationOn "jeff's home", "/mnt/sdi"
+#PPackratConfig.addName "jeff's home"
+#PPackratConfig.setBackupTarget "jeff's home", "/home/jeff"
+#PPackratConfig.setBackupDestinationOn "jeff's home", "/mnt/sdi"
 
 
 
@@ -35,11 +38,18 @@ config=PPackratConfig.new
 #This will create a directory called 'backup' in /mnt/sdi if it does not yet exist,
 #and will then create a directory called /mnt/sdi/backup/critical_docs/, the name of
 #the config, and will store your backups in there sorted by date.
-#config.addName "critical_docs"
-#config.setBackupTarget "critical_docs", "/home/jeff/Documents/"
-#config.setBackupExclusions "critical_docs", "/home/jeff/Documents/work_in_progress"
-#config.setBackupDestinationOn "critical_docs", "/mnt/sdi"
+#PPackratConfig.addName "critical_docs"
+#PPackratConfig.setBackupTarget "critical_docs", "/home/jeff/Documents/"
+#PPackratConfig.setBackupExclusions "critical_docs", "/home/jeff/Documents/work_in_progress"
+#PPackratConfig.setBackupDestinationOn "critical_docs", "/mnt/sdi"
 
+#Backup your /etc/ldap/slapd.conf file
+#
+#This will create a directory called 'backup' in '/mnt/sdi' if it does not yet exist
+#and will then create a directory called /mnt/sdi/backup/slapd , the name of the 
+#config and will store a backup of the file in there within dated directories.
+#PPackratConfig.addName 'slapd'
+#PPackratConfig.setBackupTarget 'slapd', "/etc/ldap/slapd.conf"
 
 
 #Backup your Downloads folder, set the global backup destination folder, and backup to that
@@ -50,6 +60,6 @@ config=PPackratConfig.new
 #will put your backups in there sorted by date.  If there are other backups also
 #using the global backup destination, they will be in the same /mnt/sdg/backup/
 #directory but under the name of their config not under downloads_dir.
-#config.setBackupDestination "/mnt/sdg"
-#config.addName "downloads_dir"
-#config.setBackupTarget "downloads_dir", "/home/users/Downloads"
+#PPackratConfig.setBackupDestination "/mnt/sdg"
+#PPackratConfig.addName "downloads_dir"
+#PPackratConfig.setBackupTarget "downloads_dir", "/home/users/Downloads"
