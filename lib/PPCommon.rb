736d3b3c16241de3f3db987f7ca5558b1b738a92
Jeff Welling <Jeff.Welling@Gmail.com>
2009-09-20 16:43:01 -0700

    Added require for 'find' and 'fileutils'

--- lib/PPCommon.rb
+++ lib/PPCommon.rb
@@ -15,6 +15,8 @@
     You should have received a copy of the GNU General Public License
     along with ParanoidPackrat.  If not, see <http://www.gnu.org/licenses/>.
 =end
+require 'find'
+require 'fileutils'
 
 #This is the collection of methods that are common to the ParanoidPackrat
 #project.
