9707452bccfe02cdd306c55133d4db2020abbbf0
Jeff Welling <Jeff.Welling@Gmail.com>
2009-09-21 16:41:14 -0700

    Added lib/PPIrb.rb and began writing PPIrb.simpleBackup(backup)
    lib/PPIrb.rb is intended to be the module that you interact with
    when you want to be using ParanoidPackrat from Irb.  Also, when
    you execute ./ParanoidPackrat.rb, it will be interacting with this
    same library.
    
    simpleBackup(backup) is exactly what it sounds like, a method that
    performs a simple backup based on the 'backup' it is passed from
    PPackratConfig[backupName].  More details on the process in the
    comments in the source.

---
+++ lib/PPIrb.rb
@@ -0,0 +1,33 @@
+=begin
+		Copyright 2009 Jeff Welling (jeff.welling (a) gmail.com)
+		This file is part of ParanoidPackrat.
+
+    ParanoidPackrat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ParanoidPackrat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ParanoidPackrat.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+#This module is intended to assist users while they are working with 
+#ParanoidPackrat from IRB.  Basically, the executed ./ParanoidPackrat.rb
+#file will interact with the IRB library, in a one on top of the other
+#fashion.
+module PPIrb
+	#simpleBackup(backup) performs a simple backup.
+	#backup is expected to be the hash from @Configs[name] .
+	#It will;
+	#	1. create a directory with the name of newDatetime() in the backup/name dir.
+	#	2. perform an rsync -a --link-dest=../last_backup backupTarget backup/name/datetime/ .
+	#	3. check entire backup/name dir recursively for duplicate files, and hardlink for single instance storage.
+	#
+	def self.simpleBackup(backup)
+	end
+end
