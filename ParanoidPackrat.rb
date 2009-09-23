5f02a1b369148eafc287f62a084234bd3185cef2
Jeff Welling <Jeff.Welling@Gmail.com>
2009-09-23 01:29:39 -0700

    bandaid solution to fix TicId d179d1, "Recent changes resulted in ParanoidPackrat.config.rb not loading automatically"

--- ParanoidPackrat.rb
+++ ParanoidPackrat.rb
@@ -26,6 +26,7 @@ load 'ParanoidPackrat.rb'
 load 'PPackratConfig.rb'
 load 'PPCommon.rb'
 load 'PPIrb.rb'
+load 'ParanoidPackrat.config.rb'
 
 #Just load unless we are being executed from the CLI
 if $0 == __FILE__ 
