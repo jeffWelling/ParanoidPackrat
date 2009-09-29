9969400869ae9918efc9b3832a060cdc6740e69c
wnight <wnight@gmail.com>
2009-09-29 10:48:16 -0700

    Say something on execution

--- lib/ParanoidPackrat.rb
+++ lib/ParanoidPackrat.rb
@@ -2,6 +2,7 @@ module ParanoidPackrat
   class << self
 		#Run all backups
     def run
+      puts "Performing a simple backup of each configured backup dir"
 			PPConfig.dumpConfig.each {|config|
 				PPIrb.simpleBackup(config[1])
 			}
